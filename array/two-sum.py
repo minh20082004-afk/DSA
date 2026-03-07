47355c2c5dcb0d4694c6998b89dffaa53c8f241d
minh <minh20082004@gmail.com>
2026-03-07 22:59:47 +0700

    nay quen chua luu nen lần commit đầu ko có gì trong file

---
+++ array/two-sum.py
@@ -0,0 +1,13 @@
+def twoSum(nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result = []
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    result.append(i)
+                    result.append(j)
+        return result
