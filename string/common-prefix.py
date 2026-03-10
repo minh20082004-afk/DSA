a22ad140e51ba41343d5c201c07b63d5f5352555
minh <minh20082004@gmail.com>
2026-03-11 00:53:32 +0700

    common prefix longest

---
+++ string/common-prefix.py
@@ -0,0 +1,8 @@
+def longestCommonPrefix(strs):
+    prefix = strs[0]
+    for s in strs[1:]:
+        while s[:len(prefix)]!= prefix:
+            prefix = prefix[:-1]    
+        if prefix == "":
+            return ""
+    return prefix
