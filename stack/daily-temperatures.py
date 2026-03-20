22e6dd263267caf017ebe7f2aec7fc323085f73e
minh <minh20082004@gmail.com>
2026-03-20 09:10:01 +0700

    daily-temperatures

---
+++ stack/daily-temperatures.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        res = [0] * len(temperatures)
+
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev = stack.pop()
+                res[prev] = i - prev
+            stack.append(i)
+        return res
